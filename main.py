31201f36b9ae4bc988628ce7aa4399c428955f52
BTOCC25 <BTOCC25@gmail.com>
2022-02-22 21:13:34 +0000

    feat: Model save

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import os
 
 from torch.optim import lr_scheduler
 from transform import get_default_transform
@@ -15,18 +16,43 @@ if __name__ == '__main__':
     EPOCH = 20
     BATCH_SIZE = 16
     LEARNING_RATE = 1e-3
+    criterion = nn.CrossEntropyLoss()
 
-    train_mask_data_loader = custom_data_loader(csv_path='../input/data/train/mask.csv', kind='mask', 
+    mask_data_loader = custom_data_loader(csv_path='../input/data/train/mask.csv', kind='mask', 
                                           transform=trans, train=True, batch_size=BATCH_SIZE,
                                           shuffle=True, num_workers=4)
-    custom_model = CustomNewNet(n_class=3).to(device)
-    criterion = nn.CrossEntropyLoss()
-    optimizaer = optim.Adam(custom_model.parameters(), lr=LEARNING_RATE)
-    scheduler = lr_scheduler.StepLR(optimizaer, step_size=7, gamma=0.1)
+    mask_model = CustomNewNet(n_class=3).to(device)
+    mask_optimizaer = optim.Adam(mask_model.parameters(), lr=LEARNING_RATE)
+    mask_scheduler = lr_scheduler.StepLR(mask_optimizaer, step_size=7, gamma=0.1)
 
-    result_model = train_or_eval(mode='train', model=custom_model, 
-                                data_loader=train_mask_data_loader, 
-                                criterion=criterion, optimizer=optimizaer,
-                                scheduler=scheduler, n_epoch=EPOCH, device=device)
+    age_data_loader = custom_data_loader(csv_path='../input/data/train/mask.csv', kind='age', 
+                                    transform=trans, train=True, batch_size=BATCH_SIZE,
+                                    shuffle=True, num_workers=4)
+    age_model = CustomNewNet(n_class=3).to(device)
+    age_optimizaer = optim.Adam(age_model.parameters(), lr=LEARNING_RATE)
+    age_scheduler = lr_scheduler.StepLR(age_optimizaer, step_size=7, gamma=0.1)
+
+
+    print('-' * 30)
+    print('mask train start')
+    result_mask_model = train_or_eval(mode='train', model=mask_model, 
+                                data_loader=mask_data_loader, 
+                                criterion=criterion, optimizer=mask_optimizaer,
+                                scheduler=mask_scheduler, n_epoch=EPOCH, device=device)   
+    print('-' * 30)
+    print('age train start')
+    result_age_model = train_or_eval(mode='train', model=age_model, 
+                                data_loader=age_data_loader, 
+                                criterion=criterion, optimizer=age_optimizaer,
+                                scheduler=age_scheduler, n_epoch=EPOCH, device=device)
+
+
     
-    print('Done')
+    save_path = '../model'
+    torch.save(result_mask_model.state_dict(), os.path.join(save_path, "mask_model.pt")) 
+    print('-' * 30)
+    print('Save mask Model to {}'.format(save_path))
+    print('-' * 30)
+    torch.save(result_mask_model.state_dict(), os.path.join(save_path, "age_model.pt"))
+    print('Save age Model to {}'.format(save_path))
+    print('-' * 30)
--- train.py
+++ train.py
@@ -9,9 +9,11 @@ def train_or_eval(mode, model, data_loader, criterion, optimizer, scheduler, n_e
         model.eval()   # 모델을 평가 모드로 설정
 
     since = time.time()
+    epoch_pre_time = time.time()
+    
+    print('-' * 30)
     for epoch in range(1, n_epoch + 1):
         print('Epoch {}/{}'.format(epoch, n_epoch))
-        print('-' * 10)
 
         running_loss = 0.0
         running_corrects = 0
@@ -40,7 +42,11 @@ def train_or_eval(mode, model, data_loader, criterion, optimizer, scheduler, n_e
         epoch_loss = running_loss / len(data_loader)
         epoch_acc = running_corrects.double() / total
 
+        epoch_time = time.time() - epoch_pre_time
+        epoch_pre_time = time.time()
         print('Loss: {:.4f} Acc: {:.4f}'.format(epoch_loss, epoch_acc))
+        print('Epoch {} TIME -> {:.0f}m {:.0f}s'.format(epoch, epoch_time // 60, epoch_time % 60))
+        print('-' * 30)
     
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
